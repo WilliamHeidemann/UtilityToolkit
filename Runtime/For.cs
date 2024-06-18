67bea56f216f28e274eaf16082ed734100a39aa1
William Skou Heidemann <Williamskou2227@gmail.com>
2024-06-18 14:16:22 +0200

    nested for loop

--- Runtime/For.cs
+++ Runtime/For.cs
@@ -11,11 +11,23 @@ namespace UtilityToolkit.Runtime
                 action();
             }
         }
+        
         public static void Range(int i, Action<int> action)
         {
             for (int j = 0; j < i; j++)
             {
                 action(j);
+            }
+        }
+
+        public static void NestedRange(int i, int j, Action<int, int> action)
+        {
+            for (int k = 0; k < i; k++)
+            {
+                for (int l = 0; l < j; l++)
+                {
+                    action(k, l);
+                }
             }
         }
     }
