2b9f5f5dc738c20dbc923f127855702ec276ff3a
William Skou Heidemann <Williamskou2227@gmail.com>
2024-07-02 22:02:52 +0200

    add attribute [Button] and [Button(label)] to allow calling methods from the inspector in unity

---
+++ Editor/ButtonAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace UtilityToolkit.Editor
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ButtonAttribute : PropertyAttribute
+    {
+        public string ButtonLabel { get; }
+
+        public ButtonAttribute(string buttonLabel = null)
+        {
+            ButtonLabel = buttonLabel;
+        }
+    }
+}
---
+++ Editor/ButtonEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UtilityToolkit.Editor
+{
+    [CustomEditor(typeof(MonoBehaviour), true)]
+    public class InspectorButtonEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var monoBehaviour = (MonoBehaviour)target;
+
+            // Iterate over all methods in the target class
+            var methods = monoBehaviour.GetType().GetMethods(System.Reflection.BindingFlags.Instance |
+                                                             System.Reflection.BindingFlags.Public |
+                                                             System.Reflection.BindingFlags.NonPublic);
+            foreach (var method in methods)
+            {
+                // Check if the method has the InspectorButtonAttribute
+                var attributes = method.GetCustomAttributes(typeof(ButtonAttribute), true);
+                foreach (var attribute in attributes)
+                {
+                    if (attribute is ButtonAttribute buttonAttribute)
+                    {
+                        var buttonLabel = string.IsNullOrEmpty(buttonAttribute.ButtonLabel) ? method.Name : buttonAttribute.ButtonLabel;
+
+                        if (GUILayout.Button(buttonLabel))
+                        {
+                            method.Invoke(monoBehaviour, null);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
