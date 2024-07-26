e40ec2a87d52e49defba5f61d33eeea46de672ff
William Skou Heidemann <Williamskou2227@gmail.com>
2024-07-26 09:45:43 +0200

    add CountdownTimer class

---
+++ Runtime/CountdownTimer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UtilityToolkit.Runtime
+{
+    public class CountdownTimer
+    {
+        public bool IsFinished => timeOfCreation + timeToFinish < Time.time;
+        public float FractionDone => (Time.time - timeOfCreation) / timeToFinish;
+        private readonly float timeToFinish;
+        private readonly float timeOfCreation = Time.time;
+
+        public CountdownTimer(float timeToFinish)
+        {
+            this.timeToFinish = timeToFinish;
+        }
+    }
+}
