37408478c584c0302d5e7c78c9a2e7a551d30b9e
WilliamHeidemann <Williamskou2227@gmail.com>
2024-09-29 14:19:54 +0200

    remove For.Range

--- Runtime/For.cs
+++ Runtime/For.cs
@@ -5,33 +5,6 @@ namespace UtilityToolkit.Runtime
 {
     public static class For
     {
-        public static void Range(int i, Action action)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                action();
-            }
-        }
-
-        public static void Range(int i, Action<int> action)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                action(j);
-            }
-        }
-
-        public static void NestedRange(int i, int j, Action<int, int> action)
-        {
-            for (int k = 0; k < i; k++)
-            {
-                for (int l = 0; l < j; l++)
-                {
-                    action(k, l);
-                }
-            }
-        }
-
         public static T[] GetValues<T>() where T : Enum
         {
             return Enum.GetValues(typeof(T)) as T[];
