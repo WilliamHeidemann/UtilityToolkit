92f8059beb08686ef8bd9dbabaf1c67ccf4d7e53
William Skou Heidemann <williamskou2227@gmail.com>
2024-12-27 10:49:03 +0100

    update observable

--- Runtime/Observable.cs
+++ Runtime/Observable.cs
@@ -4,23 +4,40 @@ namespace UtilityToolkit.Runtime
 {
     public class Observable<T>
     {
-        private T value;
+        private T _value;
         public event Action<T> OnValueChanged;
 
         public T Value
         {
-            get => value;
+            get => _value;
             set
             {
-                if (Equals(this.value, value)) return;
-                this.value = value;
-                OnValueChanged?.Invoke(this.value);
+                if (Equals(_value, value)) return;
+                _value = value;
+                OnValueChanged?.Invoke(_value);
             }
         }
 
         public Observable(T initialValue = default)
         {
-            value = initialValue;
+            _value = initialValue;
+        }
+        
+        public static T operator +(Observable<T> a, T b)
+        {
+            if (a.Value is int i && b is int j)
+            {
+                a.Value = (T) Convert.ChangeType(i + j, typeof(T));
+                return a.Value;
+            }
+            if (a.Value is float f && b is float g)
+            {
+                a.Value = (T) Convert.ChangeType(f + g, typeof(T));
+                return a.Value;
+            }
+            
+            a.Value = b;
+            return a.Value;
         }
     }
 }
